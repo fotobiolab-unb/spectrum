1b40a7cb31bdac20353e0e6c2c219867e099fdf4
Icaro Costa <icarosadero@gmail.com>
2022-04-19 12:58:58 -0300

    Cleaned up code and made error logs to be printed on the screen

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import requests
 import argparse
 import pandas as pd
 import os
+import re
 from urllib.parse import urljoin
 from pssh.clients import ParallelSSHClient
 from pssh.clients import SSHClient
@@ -11,6 +12,10 @@ from glob import glob
 
 path = f"{os.getcwd()}/*.ino"
 print(path)
+
+match_level = re.compile(r"level=(.*?)\s")
+match_time  = re.compile(r"time=\"(.*?)\"")
+match_msg   = re.compile(r"time=\"(.*?)\"")
 
 def get_servers(net,port="5000"):
     port_scanner = PortScanner()
@@ -29,45 +34,57 @@ def get_servers(net,port="5000"):
             pass
     return servers
 
+def print_log(rows,log):
+    for row in rows:
+        print(f"[{match_level.search(row).group(0)}]",match_msg.search(row).group(0))
+
+def run(input,clients,servers):
+    for i,client in clients.items():
+        print("Board",servers.loc[i])
+        out = client.run_command(input)
+    for row in out.stdout:
+        yield(row)
+
 if __name__=="__main__":
+    #Arguments
     parser = argparse.ArgumentParser(description="Reactor scan")
     parser.add_argument("--network", help="Network to be scanned (ex: 192.168.0.1/24)")
     parser.add_argument("--log_level", default="info")
-
     args = parser.parse_args()
 
+    #Server search via nmap
     servers = pd.DataFrame(get_servers(args.network))
     servers = servers.sort_values(by="id")
-
-    densidades = pd.read_csv("densidades.txt",sep="\t",index_col=False,header=None)
-
     clients = {row.id:SSHClient(row.addr,user="pi") for i,row in servers.iterrows()}
-
     servers.drop_duplicates(subset=['id'],inplace=True)
     servers.set_index('id',inplace=True)
 
-    print("Copying files")
+    #Auto edit densidades file
+    densidades = pd.read_csv("densidades.txt",sep="\t",index_col=False,header=None)
 
+    print("Copying files")
     for i,client in clients.items():
-        print(i)
+        print("Copying to",servers.loc[i])
         os.system(f"sed -i \"s/ID = .*;/ID = {i};/\" Spectrum.ino")
         densidade = str(densidades.iloc[int(i-1),0])
         os.system(f"sed -i \"s@/\*#\*/DensidadeAtual = .*;@/\*#\*/DensidadeAtual = {densidade}@\" h_Densidade.ino")
-        #out = client.run_command("sudo rm -r /home/pi/Spectrum")
-        #out = client.run_command("mkdir /home/pi/Spectrum; sudo chmod 0777 /home/pi/Spectrum")
-        #for file in glob(path):
-        #    client.scp_send(local_file=file,remote_file=os.path.join("/home/pi/Spectrum",os.path.basename(file)))
         client.copy_file(local_file=".",remote_file="/home/pi/Spectrum",recurse=True)
     
     print("Compiling")
     client = ParallelSSHClient(servers.addr.to_list(),user="pi")
     client.run_command("rm /home/pi/arduino-*.log")
-    output = client.run_command(f"cd /home/pi/Spectrum; /home/pi/bin/arduino-cli compile --log-file /home/pi/arduino-cli-compile.log --log-level {args.log_level} --fqbn arduino:avr:mega Spectrum")
-    print("Uploading to board")
-    for i,client in clients.items():
-        print("Board", servers.loc[i])
-        out = client.run_command(f"cd /home/pi/Spectrum; /home/pi/bin/arduino-cli upload --log-file /home/pi/arduino-cli-upload.log --log-level {args.log_level} --fqbn arduino:avr:mega -t -p {servers.loc[i].serial_port} Spectrum")
-        print(out)
-        for row in out.stdout:
-            print(row)
-    print("Done")
+
+    compile_input = f"""\
+        cd /home/pi/Spectrum;\
+        /home/pi/bin/arduino-cli compile --log-file /home/pi/arduino-cli-compile.log --log-level {args.log_level} --fqbn arduino:avr:mega Spectrum;\
+        tail /home/pi/arduino-cli-compile.log"""
+    compile_cmd = run(compile_input,clients,servers)
+    print_log(compile_cmd)
+
+    print("Uploading")
+    upload_input = f"""\
+        cd /home/pi/Spectrum;\
+        /home/pi/bin/arduino-cli upload --log-file /home/pi/arduino-cli-upload.log --log-level {args.log_level} --fqbn arduino:avr:mega -t -p {servers.loc[i].serial_port} Spectrum;\
+        tail /home/pi/arduino-cli-upload.log"""
+    upload_cmd = run(upload_input,clients,servers)
+    print_log(upload_cmd)
--- h_Densidade.ino
+++ h_Densidade.ino
@@ -38,7 +38,7 @@ Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
 byte ganhoTSL = 1;
 byte itTSL = 6;
 
-int Vis_R, Vis_B, Vis_IR;
+uint16_t Vis_R, Vis_B, Vis_IR;
 
 void configurarSensor() {
   switch (ganhoTSL) {
@@ -128,16 +128,16 @@ void medirDensidade(){
     //IR_IR = tsl.getLuminosity(TSL2591_INFRARED);
     digitalWrite(IRPin, LOW); 
  
-/*#*/DensidadeAtual = (70.45496*exp(-Vis_IR/355.07195) + 7.83826*exp(-Vis_IR/5570.96923) - 0.49460)*1E6;
+/*#*/DensidadeAtual = (70.45496*exp(-1.0*Vis_IR/355.07195) + 7.83826*exp(-1.0*Vis_IR/5570.96923) - 0.49460)*1E6;
 
 /*
-    if (ID == 1) DensidadeAtual = (71.76272*exp(-Vis_IR/354.32271) + 8.00350*exp(-Vis_IR/5506.99432) - 0.62136)*1E6;
-    if (ID == 2) DensidadeAtual = (68.10010*exp(-Vis_IR/341.86234) + 7.56218*exp(-Vis_IR/6009.17417) - 0.60030)*1E6;
-    if (ID == 3) DensidadeAtual = (69.40844*exp(-Vis_IR/312.08378) + 7.85397*exp(-Vis_IR/4884.56583) - 0.54076)*1E6;
-    if (ID == 4) DensidadeAtual = (68.81832*exp(-Vis_IR/332.10452) + 7.95428*exp(-Vis_IR/5353.53152) - 0.73229)*1E6;
-    if (ID == 5) DensidadeAtual = (72.15072*exp(-Vis_IR/325.07721) + 7.47414*exp(-Vis_IR/4989.12444) - 0.30981)*1E6;
-    if (ID == 6) DensidadeAtual = (68.10000*exp(-Vis_IR/292.42084) + 7.36802*exp(-Vis_IR/4406.68303) - 0.16793)*1E6;
-    if (ID == 7) DensidadeAtual = (70.45496*exp(-Vis_IR/355.07195) + 7.83826*exp(-Vis_IR/5570.96923) - 0.49460)*1E6;
+    if (ID == 1) DensidadeAtual = 1E6;
+    if (ID == 2) DensidadeAtual = 1E6;
+    if (ID == 3) DensidadeAtual = 1E6;
+    if (ID == 4) DensidadeAtual = 1E6;
+    if (ID == 5) DensidadeAtual = 1E6;
+    if (ID == 6) DensidadeAtual = 1E6;
+    if (ID == 7) DensidadeAtual = 1E6;
 */
   }
 }
